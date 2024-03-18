26baddff96e4d6b9ae74f3e346cb5ad000157720
HunterX18 <srsahu13@gmail.com>
2024-03-18 18:43:31 +0530

    changed server url to k8s server-service ip

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-const serverUrl = "http://localhost:5000";
+// const serverUrl = "http://localhost:5000";
 
-// const serverUrl = "http://server:5000";
+const serverUrl = "http://192.168.49.2:32669";
 export default serverUrl;
